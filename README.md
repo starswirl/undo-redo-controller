5d478b7d3cd7a95eb6357ddfb2a6c46ac744cd4a
starswirl <kzfmhpvomo@gmail.com>
2021-07-11 14:42:13 +0900

    :tada:

---
+++ README.md
@@ -0,0 +1,13 @@
+# undo-redo-controller
+
+作成中
+
+## 部品
+
+arduino Pro micro
+
+## 機能
+
+
+## 配線図
+作成中
---
+++ src/undo-redo-controller.ino
@@ -0,0 +1,36 @@
+#include <Rotary.h>
+#include "Keyboard.h"
+
+// use this option for OSX.
+// Comment it out if using Windows or Linux:
+// char ctrlKey = KEY_LEFT_GUI;
+// use this option for Windows and Linux.
+// leave commented out if using OSX:
+char ctrlKey = KEY_LEFT_CTRL;
+
+Rotary r = Rotary(2, 3);
+
+const int KEY_Z = 122;
+const int KEY_Y = 121;
+
+void undo() {
+  Keyboard.write(KEY_Z);
+}
+
+void redo() {
+  Keyboard.write(KEY_Y);
+}
+
+void setup() {
+  Serial.begin(9600);
+  r.begin(true);
+}
+
+void loop() {
+  unsigned char result = r.process();
+  if (result) {
+    Keyboard.press(ctrlKey);
+    result == DIR_CW ? redo() : undo();
+    Keyboard.release(ctrlKey);
+  }
+}
